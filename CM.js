ff9241c3047b30f59c75389989d133e839911463
Josehower <josehowerupn@gmail.com>
2020-05-04 01:21:40 +0200

    cambios en el estilo y creacion del script para crear la visualizacion adecuada del banner home

--- CM.js
+++ CM.js
@@ -1,1 +1,52 @@
-console.log('mi casa grantotota');
+const mensajes = document.querySelectorAll('.banner-home__msj ');
+const bannerHome = document.querySelectorAll('.banner-home .imagen img');
+
+const retrasoLetras = 500;
+const retrasoImagen = 150;
+const CambioFuncion = 8000;
+let actualActivo = 0;
+let imgActualActivo = 0;
+console.log(bannerHome);
+
+function wait(ms = 0) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function cambiaMensaje() {
+  if (actualActivo === 2) {
+    mensajes[actualActivo].classList.remove('active');
+    await wait(retrasoLetras);
+    mensajes[actualActivo].classList.add('none');
+    actualActivo = 0;
+    mensajes[actualActivo].classList.remove('none');
+    await wait(retrasoLetras);
+    mensajes[actualActivo].classList.add('active');
+  } else {
+    mensajes[actualActivo].classList.remove('active');
+    await wait(retrasoLetras);
+    mensajes[actualActivo].classList.add('none');
+    actualActivo += 1;
+    mensajes[actualActivo].classList.remove('none');
+    await wait(retrasoLetras);
+    mensajes[actualActivo].classList.add('active');
+  }
+}
+
+async function cambiaFondo() {
+  if (imgActualActivo === 2) {
+    bannerHome[imgActualActivo].classList.remove('active');
+    await wait(retrasoImagen);
+    imgActualActivo = 0;
+    bannerHome[imgActualActivo].classList.add('active');
+  } else {
+    bannerHome[imgActualActivo].classList.remove('active');
+    await wait(retrasoImagen);
+    imgActualActivo += 1;
+    bannerHome[imgActualActivo].classList.add('active');
+  }
+}
+
+window.setInterval(cambiaMensaje, CambioFuncion);
+window.setInterval(cambiaFondo, CambioFuncion);
